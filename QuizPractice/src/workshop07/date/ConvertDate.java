2d25e3f6ddb381d2462873a439938089176e188b
jmboy713 <jimin713@naver.com>
2024-04-05 18:21:57 +0900

    quiz 07-6번 해결 ㅌ

--- QuizPractice/src/workshop07/date/ConvertDate.java
+++ QuizPractice/src/workshop07/date/ConvertDate.java
@@ -1,22 +1,38 @@
 package workshop07.date;
 
+import java.text.SimpleDateFormat;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.HashMap;
 
 public class ConvertDate {
     private LocalDate date;
     private int num;
-    public static String convert(LocalDate date, int a) {
+    public static String convert(LocalDateTime date, int a) {
         HashMap<DayOfWeek, String> map = new HashMap<>();
         map.put(DayOfWeek.MONDAY, "월요일");
+        map.put(DayOfWeek.TUESDAY, "화요일");
+        map.put(DayOfWeek.WEDNESDAY, "수요일");
+        map.put(DayOfWeek.THURSDAY, "목요일");
+        map.put(DayOfWeek.FRIDAY, "금요일");
+        map.put(DayOfWeek.SATURDAY, "토요일");
+        map.put(DayOfWeek.SUNDAY, "일요일");
+        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat sdf2 = new SimpleDateFormat("yy년 MM월 dd일");
+        SimpleDateFormat sdf3 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
         if (a == 1) {
-            return date.getYear() + "-" + date.getMonthValue() + "-" + date.getDayOfMonth();
+            return sdf1.format(date);
         } else if (a == 2) {
-            return date.getYear() + "년 " + date.getMonthValue() + "월" + date.getDayOfMonth() + "일 " + date.getDayOfWeek();
+            return sdf2.format(date) + map.get(date.getDayOfWeek());
         } else if (a == 3) {
-            return "dd";
+            if (date.getHour() >= 12) {
+                return sdf3.format(date)+"오후";
+                
+            }
+            return sdf3.format(date)+"오전";
 
         }return "Type에 없습니다.";
 
@@ -28,5 +44,7 @@ public class ConvertDate {
     public ConvertDate(LocalDate date, int num) {
         this.date = date;
         this.num = num;
-    }
+     }
 }
+
+
--- QuizPractice/src/workshop07/date/DateTest.java
+++ QuizPractice/src/workshop07/date/DateTest.java
@@ -1,6 +1,7 @@
 package workshop07.date;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import static workshop07.date.ConvertDate.convert;
 
@@ -8,8 +9,8 @@ public class DateTest {
     public static void main(String[] args) {
 
         ConvertDate convertDate = new ConvertDate();
-        System.out.println(convert(LocalDate.now(),1));
-        System.out.println(convert(LocalDate.now(),2));
+        System.out.println(convert(LocalDateTime.now(),1));
+        System.out.println(convert(LocalDateTime.now(),2));
 
     }
 }
